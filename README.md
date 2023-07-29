5640600e5427ceab26ccb70f1f979ff235c9868c
jchurch133 <jchurch133@gmail.com>
2023-07-28 21:49:02 -0500

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Ubuntu-Startup-Template
 Using Kubuntu, bash, loginctl, xdotool, and pactl, I launch several startup functions to set volume levels, play Spotify, and open apps.
+
---
+++ old-autostart-scripts/startup_apps.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Call the functions in the desired order
+main() {
+    launch_vscode
+    launch_google_chrome
+    launch_spotify
+    set_default_volume
+    check_user_status
+    delayed_login_loop
+    simulate_play_button
+    set_spotify_volume
+    set_default_volume_back
+    restore_chrome
+    open_sound_volume_control
+}
+
+# Function to launch VSCode
+launch_vscode() {
+    code &
+    sleep 1
+}
+
+# Function to launch Google Chrome
+launch_google_chrome() {
+    google-chrome &
+    sleep 1
+}
+
+# Function to launch Spotify
+launch_spotify() {
+    spotify &
+    sleep 1
+}
+
+# Function to set default volume
+set_default_volume() {
+    pactl set-sink-volume @DEFAULT_SINK@ 30% &
+    sleep 1
+}
+
+# Function to check user status
+check_user_status() {
+    if loginctl user-status $(id -u) | grep -q "State: active"; then
+        echo "User is active"
+        return 0
+    else
+        echo "User is inactive"
+        return 1
+    fi
+}
+
+# Wait until user is logged in
+delayed_login_loop() {
+    while ! check_user_status; do
+        sleep 5
+    done
+}
+
+# Function to simulate pressing the "play" button in Spotify (identify and focus spotify window, press spacebar)
+simulate_play_button() {
+    spotify_window_id=$(xdotool search --onlyvisible --name "Spotify")
+    xdotool windowactivate --sync "$spotify_window_id" key space &
+    sleep 6
+}
+
+# Function to set the volume for Spotify (identify the sink # for spotify; can adjust the percentage as desired)
+set_spotify_volume() {
+    spotify_sink_input=$(pactl list sink-inputs | grep -B 17 -i "spotify" | grep -oP "Sink Input #\K\d+")
+    pactl set-sink-input-volume "$spotify_sink_input" 30% &
+    sleep 3
+}
+
+# Function to set default volume back to normal
+set_default_volume_back() {
+    pactl set-sink-volume @DEFAULT_SINK@ 100% &
+    sleep 2
+}
+
+# Function to restore Chrome Windows (focuses Chrome window then presses Enter)
+restore_chrome() {
+    chrome_window_id=$(xdotool search --onlyvisible --name "Google Chrome")
+    xdotool windowactivate --sync "$chrome_window_id" key Return &
+    sleep 5
+}
+
+# Function to open the sound volume control (using a manually created shortcut in Kubuntu Linux)
+open_sound_volume_control() {
+    xdotool keydown Super_L key v keyup Super_L
+}
+
+# Call the main function
+main
+
+
