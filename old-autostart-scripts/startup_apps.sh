5dbe3f2778713ef7c29106edf4887b9c078a0de8
jchurch133 <jchurch133@gmail.com>
2023-07-29 21:33:01 -0500

    Added thunderbird, simplified google lauch into one cmd, adjusted some sleep timers, open volume control still a little buggy

--- old-autostart-scripts/startup_apps.sh
+++ old-autostart-scripts/startup_apps.sh
@@ -3,7 +3,7 @@
 # Call the functions in the desired order
 main() {
     launch_vscode
-    launch_google_chrome
+    launch_thunderbird
     launch_spotify
     set_default_volume
     check_user_status
@@ -11,7 +11,7 @@ main() {
     simulate_play_button
     set_spotify_volume
     set_default_volume_back
-    restore_chrome
+    launch_google_chrome
     open_sound_volume_control
 }
 
@@ -21,9 +21,9 @@ launch_vscode() {
     sleep 1
 }
 
-# Function to launch Google Chrome
-launch_google_chrome() {
-    google-chrome &
+# Function to launch Thunderbird mail client
+launch_thunderbird() {
+    thunderbird &
     sleep 1
 }
 
@@ -53,7 +53,7 @@ check_user_status() {
 # Wait until user is logged in
 delayed_login_loop() {
     while ! check_user_status; do
-        sleep 5
+        sleep 4
     done
 }
 
@@ -67,35 +67,34 @@ delayed_login_loop() {
 simulate_play_button() {
     spotify_window_id=$(xdotool search --onlyvisible --name "Spotify")
     xdotool windowactivate --sync "$spotify_window_id" key space &
-    sleep 6
+    sleep 5
 }
 
 # Function to set the volume for Spotify (identify the sink # for spotify; can adjust the percentage as desired)
 set_spotify_volume() {
     spotify_sink_input=$(pactl list sink-inputs | grep -B 17 -i "spotify" | grep -oP "Sink Input #\K\d+")
     pactl set-sink-input-volume "$spotify_sink_input" 30% &
-    sleep 3
+    sleep 1
 }
 
 # Function to set default volume back to normal
 set_default_volume_back() {
     pactl set-sink-volume @DEFAULT_SINK@ 100% &
-    sleep 2
+    sleep 1
 }
 
-# Function to restore Chrome Windows (focuses Chrome window then presses Enter)
-restore_chrome() {
-    chrome_window_id=$(xdotool search --onlyvisible --name "Google Chrome")
-    xdotool windowactivate --sync "$chrome_window_id" key Return &
-    sleep 5
+# Function to launch Google Chrome
+launch_google_chrome() {
+    google-chrome
+    sleep 1
+    xdotool key Tab Tab Tab Tab Return &
+    sleep 2
 }
 
 # Function to open the sound volume control (using a manually created shortcut in Kubuntu Linux)
 open_sound_volume_control() {
-    xdotool keydown Super_L key v keyup Super_L
+    xdotool key Escape keydown Super_L key v keyup Super_L
 }
 
 # Call the main function
 main
-
-
